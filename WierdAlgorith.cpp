c0b2362bca33a17f3196778ae9a1e6f6ca2f752f
snigdha287 <97682411+snigdha287@users.noreply.github.com>
2022-07-27 20:09:13 +0530

    Update WierdAlgorith.cpp

--- WierdAlgorith.cpp
+++ WierdAlgorith.cpp
@@ -34,14 +34,14 @@ int main()
     cout<<n;
     while(n>1)
     {
-        if(n%2==0)
-        {
-            n=n/2;
-            cout<<"\t"<<n;
-        }
-        else if(n%2==1)
+        if(n%2==1)
         {
             n=n*3+1;
+            cout<<"\t"<<n;
+        }
+        else  
+        {
+            n=n/2;
             cout<<"\t"<<n;
         }
     }
