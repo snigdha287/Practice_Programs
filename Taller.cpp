225220559809a0f680d2116f158e4fb0f2bf735c
snigdha287 <97682411+snigdha287@users.noreply.github.com>
2022-08-01 21:22:31 +0530

    Update Taller.cpp

--- Taller.cpp
+++ Taller.cpp
@@ -29,13 +29,10 @@ B
 A
 */
 
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
 	    public static void main (String[] args) throws java.lang.Exception
