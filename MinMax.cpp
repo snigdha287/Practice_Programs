eada77387bac0b5325e4461ae8ccc7ba3b51144c
snigdha287 <97682411+snigdha287@users.noreply.github.com>
2022-08-02 21:10:06 +0530

    Create MinMax.cpp

---
+++ MinMax.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr;
+    int a,min=0,max=0;
+    for (int i = 0; i < 5; i++) 
+    {
+        cin>>a;
+        arr.push_back(a);   
+    }
+    sort(arr.begin(),arr.end());
+    for (auto i = arr.begin(); i != arr.end()-1; ++i)
+        min+=*i;
+    for (auto i = arr.begin()+1; i != arr.end(); ++i)
+        max+=*i;
+    cout<<min<<" "<<max<<endl;
+    return 0;
+}
