aa874538468a8fcfe7362cbbe910a58503803154
snigdha287 <97682411+snigdha287@users.noreply.github.com>
2022-08-04 06:34:04 +0530

    Create Discount.java

---
+++ Discount.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int T,x;
+		Scanner s =new Scanner(System.in);
+		T= s.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		    x= s.nextInt();
+            System.out.println(100-x);
+		}
+	}
+}
